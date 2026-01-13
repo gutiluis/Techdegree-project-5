2aaa86be8f18df6ca8ed8dc056b65162e4918ff4
gutiluis <gutierrez.luis@icloud.com>
2026-01-12 17:46:09 -0800

    push

--- README.md
+++ README.md
@@ -92,6 +92,7 @@
 
 # html:
     - the same name of the form attribute should be used with sqlalchemy when creating the model object instance
+    - within the form the required attribute does not affect the nullable=false within the sqlalchemy model
 
 # http:
     - 302 found request post, 200, 304
@@ -103,13 +104,59 @@
     - sqlite3 db_file.db
     - .tables
     - select * from table_name;
+    - .schema table_name;
+    - PRAGMA table_info(projects);
     
-    # update sqlitedb with or with transactions
+    # update sqlitedb with or without transactions
     - UPDATE project
     - SET id = '2'
     - WHERE id = 1;
 
 
+    # safer to do a transaction directly sqlite3 in db form cli
+    BEGIN;
+
+    UPDATE projects
+    SET github_repo = 'https://github.com/gutiluis/Techdegree-project-5'
+    WHERE id = 1;
+    # check
+    SELECT id, github_repo FROM projects WHERE id = 1;
+
+    COMMIT;
+
+
+    # use not null to force the column always have input
+
+# aplying an href to an anchor changes the font. the goal is using an url_for flask route
+
+
+# difference between string parse time and string parse format time
+model needs a datetime object. strptime
+
+# get appearse in the browser. post doesnt
+
+# using strftime within jinja template to display only year and month
+
+
+# value does not work with jinja from the form in textarea tags
+In HTML, the initial content of a <textarea> is specified between its opening and closing tags, not as a value attribute.
+
+# flask error handling not for normal control flow
+# not to replace validation
+# not to hide bugs permanently
+# error handling in flask is for exceptions
+
+# request in flask
+class flask.Request(environ, populate_request=True, shallow=False)¶
+property form: ImmutableMultiDict[str, str]
+The form parameters. By default an ImmutableMultiDict is returned from this function. This can be changed by setting parameter_storage_class to a different type. This might be necessary if the order of the form data is important.
+
+# get_or_404(ident(Any), description=None)
+Like get() but aborts with a 404 Not Found error instead of returning None.
+
+
+
+https://flask.palletsprojects.com/en/2.2.x/errorhandling/?highlight=error%20page#custom-error-pages
 -----
 
 ##
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import render_template, url_for, request, redirect
+from datetime import datetime
 from models import db, Project, app
 
 
@@ -10,40 +11,51 @@ def index():
 
 @app.route('/projects/new', methods=["GET", "POST"])
 def add_project():
-    if request.form:
+    if request.method == "POST":
+
+        year_and_month = request.form.get('date')
+        # model takes datetimeobject in column
+        convert_string = datetime.strptime(year_and_month, "%Y-%m").date()
+
         new_project = Project(
-            title=request.form['title'],
-            description=request.form['desc'],
-            skills=request.form['skills'],
-            github_repo=request.form['github'])
+            # get is a method needs parentheses
+            title=request.form.get('title'),
+            created=convert_string,
+            description=request.form.get('desc'),
+            skills=request.form.get('skills'),
+            github_repo=request.form.get('github'))
+
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('projectform.html')
 
 
-# test
-@app.route('/project_detail/<id>')
+@app.route('/projects/<id>')
 def project_detail(id):
     project = Project.query.get_or_404(id)
     return render_template('detail.html', project=project)
 
 
-# test
 @app.route('/projects/<id>/edit', methods=["GET", "POST"])
 def project_edit(id):
+    """ same conventions from add project"""
     project = Project.query.get_or_404(id)
-    if request.form:
-        project.title = request.form["title"] # created is discontinued
-        project.description = request.form['desc']
-        project.skills = request.form['skills']
-        project.github_repo = request.form['github']
+    if request.method == 'POST':
+
+        year_and_month = request.form.get('date')
+        convert_string = datetime.strptime(year_and_month, "%Y-%m").date()
+
+        project.title = request.form.get('title')
+        project.created = convert_string
+        project.description = request.form.get('desc')
+        project.skills = request.form.get('skills')
+        project.github_repo = request.form.get('github')
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('editform.html', project=project) # the edit.html is the same as projectform.html. new is empty and edit with existing data
+    return render_template('editform.html', project=project)
 
 
-# test
 @app.route('/projects/<id>/delete')
 def project_delete(id):
     project = Project.query.get_or_404(id)
@@ -51,17 +63,18 @@ def project_delete(id):
     db.session.commit()
     return redirect(url_for('index'))
 
+
 @app.route('/about')
-def project_about():
+def about_me():
     return render_template('about.html')
 
-# test
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', msg=error), 404
 
 
 if __name__ == "__main__":
-    with app.app_context(): # proxy
+    with app.app_context():  # proxy
         db.create_all()
     app.run(debug=True, port=9000, host='127.0.0.1')
